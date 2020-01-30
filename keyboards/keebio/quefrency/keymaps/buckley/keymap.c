aff8e4a846852a0c4922cbb06b91445d660e1fd7
Michael Buckley <michael.buckley@cordatahealth.com>
2020-01-30 13:54:45 -0500

    update keymap

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -1,11 +1,10 @@
+// Note from Buckley:
+// - to install from linux, you need to run `systemctl stop ModemManager.service`
+
 #include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BASE 0
 #define _FN0 1
 #define _FN1 2
@@ -26,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN0] = LAYOUT_65_with_macro(
-    KC_F11,  KC_F12,  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
-    KC_F13,  KC_F14,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_F15,  KC_F16,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    KC_F11,  KC_F12,  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_PSCR, \
+    KC_F13,  KC_F14,  _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    KC_F15,  KC_F16,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______, \
     KC_F17,  KC_F18,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     KC_F19,  KC_F20,  _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   ),
