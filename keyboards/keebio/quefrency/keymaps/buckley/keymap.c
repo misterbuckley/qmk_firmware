307dd212426bc57d54732a4fdbbc038b6faba481
Michael Buckley <michael.buckley@cordatahealth.com>
2020-02-06 10:50:35 -0500

    add power hotkey and fn+Lspace=enter

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -1,5 +1,9 @@
-// Note from Buckley:
-// - to install from linux, you need to run `systemctl stop ModemManager.service`
+// Note from Buckley: to flash from linux, you need to run the following:
+//     `systemctl stop ModemManager.service`
+//     `sudo make keebio/quefrency/rev1:buckley:avrdude`
+
+
+// keycodes https://beta.docs.qmk.fm/features/keycodes_basic
 
 #include QMK_KEYBOARD_H
 
@@ -21,23 +25,24 @@ enum custom_keycodes {
 
 enum {
   TD_LOCK = 0,
+  TD_PWR,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_VOLD,     KC_VOLU, KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
-    KC_MPLY,     KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_MPRV,     KC_MNXT, KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGUP, \
-    WSCRSHT,     SSCRSHT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,   KC_PGDN, \
-    TD(TD_LOCK), _______, KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0),KC_SPC,           KC_SPC,  _______, MO(_FN1), MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_VOLD,     KC_VOLU,    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
+    KC_MPLY,     KC_MUTE,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    KC_MPRV,     KC_MNXT,    KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGUP, \
+    WSCRSHT,     SSCRSHT,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,   KC_PGDN, \
+    TD(TD_LOCK), TD(TD_PWR), KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0),KC_SPC,           KC_SPC,  _______, MO(_FN1), MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN0] = LAYOUT_65_with_macro(
     _______, _______, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
-    _______, _______, _______, _______, KC_UP,   _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, KC_BSPC, KC_UP,   KC_DEL,  _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_4,    KC_5,    KC_6,    _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,          KC_0,    _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, KC_ENT,           KC_0,    _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
@@ -66,5 +71,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(_______, LOCKCMP)
+  [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(_______, LOCKCMP),
+  [TD_PWR] = ACTION_TAP_DANCE_DOUBLE(_______, KC_POWER),
 };
