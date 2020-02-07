aa1814f4b830bf4de57007d7ad24774ad0b7cf0b
Michael Buckley <michael.buckley@cordatahealth.com>
2020-02-07 08:57:02 -0500

    hibernate is useless in ubuntu so remove this

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -25,16 +25,15 @@ enum custom_keycodes {
 
 enum {
   TD_LOCK = 0,
-  TD_PWR,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_VOLD,     KC_VOLU,    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
-    KC_MPLY,     KC_MUTE,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_MPRV,     KC_MNXT,    KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGUP, \
-    WSCRSHT,     SSCRSHT,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,   KC_PGDN, \
-    TD(TD_LOCK), TD(TD_PWR), KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0),KC_SPC,           KC_SPC,  _______, MO(_FN1), MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_VOLD,     KC_VOLU, KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
+    KC_MPLY,     KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    KC_MPRV,     KC_MNXT, KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGUP, \
+    WSCRSHT,     SSCRSHT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,   KC_PGDN, \
+    TD(TD_LOCK), _______, KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0),KC_SPC,           KC_SPC,  _______, MO(_FN1), MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN0] = LAYOUT_65_with_macro(
@@ -71,6 +70,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(_______, LOCKCMP),
-  [TD_PWR] = ACTION_TAP_DANCE_DOUBLE(_______, KC_POWER),
+  [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(KC_NLCK, LOCKCMP),
 };
