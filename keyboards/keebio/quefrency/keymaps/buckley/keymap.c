1461aa403358088356b2ba818585d372fa20e94b
Michael Buckley <michael.buckley@cordatahealth.com>
2020-02-18 16:30:28 -0500

    stuff

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -19,8 +19,6 @@ extern keymap_config_t keymap_config;
 #define SSCRSHT LSFT(KC_PSCR)
 #define GO_BACK LALT(KC_LEFT)
 #define GO_FWRD LALT(KC_RGHT)
-#define TABLEFT LSFT(LCTL(KC_TAB))
-#define TABRGHT LCTL(KC_TAB)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -32,26 +30,26 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_VOLD,     KC_VOLU, KC_GESC, KC_1,    KC_2,    KC_3,     KC_4,   KC_5, KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
-    KC_MPLY,     KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,   KC_T, KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_MPRV,     KC_MNXT, KC_LGUI, KC_A,    KC_S,    KC_D,     KC_F,   KC_G, KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
-    WSCRSHT,     SSCRSHT, KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,   KC_B, KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    TD(TD_LOCK), _______, KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0), KC_SPC,       LT(_FN1, KC_SPC), XXXXXXX, TG(_FN0), MO(_FN1), _______, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_VOLD,     KC_VOLU, KC_GESC, KC_1,    KC_2,    KC_3,     KC_4,   KC_5, KC_6,             KC_7,    KC_8,     KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
+    KC_MPLY,     KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,   KC_T, KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    KC_MPRV,     KC_MNXT, KC_LGUI, KC_A,    KC_S,    KC_D,     KC_F,   KC_G, KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+    WSCRSHT,     SSCRSHT, KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,   KC_B, KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+    TD(TD_LOCK), _______, KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0), KC_SPC,       LT(_FN1, KC_SPC), XXXXXXX, TG(_FN0), TG(_FN1), _______, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN0] = LAYOUT_65_with_macro(
     _______, _______, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
-    _______, _______, _______, GO_BACK, KC_UP,   GO_FWRD, _______, _______, KC_7,  KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, KC_UP,   _______, _______, _______, KC_7,  KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_4,  KC_5,    KC_6,    _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_1,  KC_2,    KC_3,    _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, KC_ENT,           KC_0,  XXXXXXX, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______,          KC_0,  XXXXXXX, _______, _______, _______, _______, _______, _______
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_BTN2, KC_WH_D, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, GO_BACK, KC_BTN1, KC_BTN2, GO_FWRD, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, GO_BACK, GO_FWRD, TABLEFT, TABRGHT, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_D, KC_WH_L, KC_WH_R, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______,          _______, XXXXXXX, _______, _______, _______, _______, _______, _______
   ),
 
