55be326704b0fa27db0c4edd98706e3863259eab
Michael Buckley <michael.buckley@cordatahealth.com>
2020-03-02 11:00:41 -0500

    mouse scroll like hjkl

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -25,18 +25,17 @@ enum custom_keycodes {
 };
 
 enum {
-  TD_LOCK = 0,
+  TD_LOCK,
   TD_SHOT,
   TD_MAC1,
   TD_MAC2,
-  TD_VOL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    TD(TD_VOL),  KC_VOLU,     KC_GESC, KC_1,    KC_2,    KC_3,     KC_4,   KC_5, KC_6,             KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
-    KC_MPRV,     KC_MNXT,     KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,   KC_T, KC_Y,             KC_U,    KC_I,        KC_O,        KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_MPLY,     _______,     KC_LGUI, KC_A,    KC_S,    KC_D,     KC_F,   KC_G, KC_H,             KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+    KC_MPLY,     KC_MUTE,     KC_GESC, KC_1,    KC_2,    KC_3,     KC_4,   KC_5, KC_6,             KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
+    KC_VOLD,     KC_VOLU,     KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,   KC_T, KC_Y,             KC_U,    KC_I,        KC_O,        KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    KC_MPRV,     KC_MNXT,     KC_LGUI, KC_A,    KC_S,    KC_D,     KC_F,   KC_G, KC_H,             KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     _______,     _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,   KC_B, KC_N,             KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
     TD(TD_LOCK), TD(TD_SHOT), KC_LGUI, KC_LALT, KC_LCTL, MO(_FN0), KC_SPC,       LT(_FN1, KC_SPC), XXXXXXX, TD(TD_MAC1), TD(TD_MAC2), DM_RSTP, KC_LEFT, KC_DOWN, KC_RGHT
   ),
@@ -53,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN3, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, GO_BACK, KC_BTN1, KC_BTN2, GO_FWRD, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_D, KC_WH_L, KC_WH_R, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______,          _______, XXXXXXX, _______, _______, _______, _______, _______, _______
   ),
 
@@ -103,9 +102,8 @@ void dynamicMacro2(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(KC_LOCK, LOCKCMP),
+  [TD_LOCK] = ACTION_TAP_DANCE_DOUBLE(_______, LOCKCMP),
   [TD_SHOT] = ACTION_TAP_DANCE_DOUBLE(SELSHOT, WINSHOT),
   [TD_MAC1] = ACTION_TAP_DANCE_FN(dynamicMacro1),
   [TD_MAC2] = ACTION_TAP_DANCE_FN(dynamicMacro2),
-  [TD_VOL] = ACTION_TAP_DANCE_DOUBLE(KC_VOLD, KC_MUTE),
 };
