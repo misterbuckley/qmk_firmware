1ff4671f6084aedb92369cff1d891a4e2132817e
Michael Buckley <michael.buckley@cordatahealth.com>
2020-03-08 22:01:50 -0400

    add mouse to left side

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -30,11 +30,11 @@ enum tap_dances {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_65_with_macro(
-    KC_MPLY,     KC_MUTE,     KC_GESC, KC_1,    KC_2,    KC_3,       KC_4,   KC_5, KC_6,              KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
-    KC_VOLD,     KC_VOLU,     KC_TAB,  KC_Q,    KC_W,    KC_E,       KC_R,   KC_T, KC_Y,              KC_U,    KC_I,        KC_O,        KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_MPRV,     KC_MNXT,     KC_LGUI, KC_A,    KC_S,    KC_D,       KC_F,   KC_G, KC_H,              KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
-    _______,     _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,   KC_B, KC_N,              KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    TD(TD_LOCK), TD(TD_SHOT), KC_LGUI, KC_LALT, KC_LCTL, MO(NUMPAD), KC_SPC,       LT(MOUSE, KC_SPC), XXXXXXX, TD(TD_MAC1), TD(TD_MAC2), DM_RSTP, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MPLY,     KC_MUTE,     KC_GESC, KC_1,    KC_2,    KC_3,       KC_4,   KC_5,      KC_6,              KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
+    KC_VOLD,     KC_VOLU,     KC_TAB,  KC_Q,    KC_W,    KC_E,       KC_R,   KC_T,      KC_Y,              KC_U,    KC_I,        KC_O,        KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+    KC_MPRV,     KC_MNXT,     KC_LGUI, KC_A,    KC_S,    KC_D,       KC_F,   KC_G,      KC_H,              KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+    _______,     _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,   KC_B,      KC_N,              KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+    TD(TD_LOCK), TD(TD_SHOT), KC_LGUI, KC_LALT, KC_LCTL, MO(NUMPAD), LT(MOUSE, KC_SPC), LT(MOUSE, KC_SPC), XXXXXXX, TD(TD_MAC1), TD(TD_MAC2), DM_RSTP, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [NUMPAD] = LAYOUT_65_with_macro(
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [MOUSE] = LAYOUT_65_with_macro(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN3, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, GO_BACK, KC_BTN1, KC_BTN2, GO_FWRD, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______, \
+    _______, _______, _______, _______, GO_BACK, KC_BTN2, KC_BTN1, GO_FWRD, GO_BACK, KC_BTN1, KC_BTN2, GO_FWRD, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______, \
+    _______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______,          _______, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______
   ),
 
