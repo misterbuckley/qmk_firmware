acaf1d89505b4cfc4c5eed645c8f1c0d3280a909
Michael Buckley <michael.buckley@cordatahealth.com>
2020-03-28 22:45:56 -0400

    add layer for mod keys on right side

--- keyboards/keebio/quefrency/keymaps/buckley/keymap.c
+++ keyboards/keebio/quefrency/keymaps/buckley/keymap.c
@@ -16,7 +16,8 @@ extern keymap_config_t keymap_config;
 
 enum layers {
   BASE,
-  WADS,
+  MOD_WIN,
+  FN,
   NUMPAD,
   MOUSE,
   MACRO,
@@ -32,18 +33,26 @@ enum tap_dances {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_65_with_macro(
     KC_VOLD,     KC_VOLU,     KC_GESC, KC_1,    KC_2,    KC_3,       KC_4,   KC_5,      KC_6,              KC_7,    KC_8,        KC_9,        KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_DEL, KC_HOME, \
-    KC_MPLY,     KC_MUTE,     KC_TAB,  KC_Q,    KC_W,    KC_E,       KC_R,   KC_T,      KC_Y,              KC_U,    KC_I,        KC_O,        KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_MPRV,     KC_MNXT,     KC_LCTL, KC_A,    KC_S,    KC_D,       KC_F,   KC_G,      KC_H,              KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
-    _______,     _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,   KC_B,      KC_N,              KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    TD(TD_LOCK), TD(TD_SHOT), KC_LCTL, KC_LALT, KC_LGUI, MO(WADS), KC_SPC,            LT(MOUSE, KC_SPC), XXXXXXX, TD(TD_MAC1), TD(TD_MAC2), DM_RSTP, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MPLY,     KC_MUTE,     KC_TAB,  KC_Q,    KC_W,    KC_E,       KC_R,   KC_T,      KC_Y,              KC_U,    KC_I,        KC_O,        KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, \
+    KC_MPRV,     KC_MNXT,     KC_LCTL, KC_A,    KC_S,    KC_D,       KC_F,   KC_G,      KC_H,              KC_J,    KC_K,        KC_L,        KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN, \
+    _______,     _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,   KC_B,      KC_N,              KC_M,    KC_COMM,     KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   KC_END, \
+    TD(TD_LOCK), TD(TD_SHOT), KC_LCTL, KC_LALT, KC_LGUI, MO(FN),     KC_SPC,            LT(MOUSE, KC_SPC), XXXXXXX, TD(TD_MAC1), TD(TD_MAC2), DM_RSTP, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [WADS] = LAYOUT_65_with_macro(
-    _______, _______, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
-    _______, _______, _______, _______, KC_UP,   _______, _______, _______, KC_7,  KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_4,  KC_5,    KC_6,    _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_1,  KC_2,    KC_3,    _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,          KC_0,  XXXXXXX, _______, _______, _______, _______, _______, _______
+  [MOD_WIN] = LAYOUT_65_with_macro(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, XXXXXXX, KC_RGUI, KC_RALT, KC_RCTL, _______, _______, _______
+  ),
+
+  [FN] = LAYOUT_65_with_macro(
+    _______, _______, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_F7,   KC_F8,       KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
+    _______, _______, _______, KC_PGDN, KC_UP,   KC_PGUP, _______, _______, KC_7,  KC_8,    KC_9,        _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_4,  KC_5,    KC_6,        _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_1,  KC_2,    KC_3,        _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          KC_0,  XXXXXXX, TG(MOD_WIN), _______, _______, _______, _______, _______
   ),
 
   [NUMPAD] = LAYOUT_65_with_macro(
